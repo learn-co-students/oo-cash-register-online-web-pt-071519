8b5bc4b46f00856aa3c958a5761782d9218b94c7
assane savadogo <assane.savadogo81@gmail.com>
2019-08-29 00:56:38 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,38 +1,37 @@
 require 'pry'
 
 class CashRegister
-  attr_accessor :total, :discount
-  
-  def initialize(discount = nil)
-    @total = 0 
-    @discount = discount 
-    @items = []
-  end
-  
-  def add_item(title, price, quantity = 1)
-    self.total += price * quantity 
-    quantity.times do 
-      @items << title
-    end
-    @last_item = price * quantity
-  end
-  
-  def apply_discount 
-    if discount 
-      self.total -= (self.total.to_f * discount/ 100).to_i
-      "After the discount, the total comes to $#{@total}."
-    else 
-      "There is no discount to apply."
-    end
-  end
-  
-  def items 
-    @items
-  end
-  
-  def void_last_transaction
-    @items.pop 
-    self.total -= @last_item
-  end
-  
+ attr_accessor :total, :discount, :last_transaction
+ 
+ def initialize(discount = nil)
+   @discount = discount
+   @total = 0
+   @items = []
+ end
+ 
+ def add_item(title, price, quantity = 1)
+   @total += price * quantity
+   quantity.times do 
+     @items << title
+   end 
+   @last_transaction = price *quantity
+ end
+ 
+ def apply_discount
+   if discount
+     @total -= (self.total.to_f * discount/ 100).to_i
+     "After the discount, the total comes to $#{@total}."
+   else 
+     "There is no discount to apply."
+   end
+ end
+ 
+ def items 
+   @items 
+ end
+ 
+ def void_last_transaction
+   @items.pop
+   @total -= @last_transaction
+ end
 end
